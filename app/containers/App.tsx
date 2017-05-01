3f1f2c54c936cb86a5220632ed7bdad6fd00eec7
samuelmaddock <samuel.maddock@gmail.com>
2017-05-01 01:05:51 -0400

    Rename js to ts extensions

--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,11 +1,6 @@
-import React, { Component } from 'react';
-import type { Children } from 'react';
+import React, { Component, Children } from 'react';
 
 export default class App extends Component {
-  props: {
-    children: Children
-  };
-
   render() {
     return (
       <div>
