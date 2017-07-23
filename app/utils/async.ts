aee925624f0a44be3f0482d4bc0ba8e0b51e448f
samuelmaddock <samuel.maddock@gmail.com>
2017-07-23 14:39:15 -0400

    Add Deferred async object

--- app/utils/async.ts
+++ app/utils/async.ts
@@ -3,3 +3,32 @@ export const sleep = (time: number) => {
     setTimeout(resolve, time);
   });
 }
+
+/**
+ * Wraps a promise to be resolved at a later time.
+ */
+export class Deferred<T> {
+  promise: Promise<T>;
+  resolved: boolean = false;
+  rejected: boolean = false;
+
+  private _resolve: (value: T | PromiseLike<T> | undefined) => void;
+  private _reject: (reason: any) => void;
+
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
+
+  resolve(value: T | PromiseLike<T> | undefined): void {
+    this.resolved = true;
+    this._resolve(value);
+  }
+
+  reject(reason: any): void {
+    this.rejected = true;
+    this.reject(reason);
+  }
+}
