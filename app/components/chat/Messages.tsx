16e69668094e57ad1e9a124b6590b708f655535d
Austin Carr <austin.r.carr@gmail.com>
2017-09-18 23:09:45 -0400

    WIP

--- app/components/chat/Messages.tsx
+++ app/components/chat/Messages.tsx
@@ -7,12 +7,59 @@ interface IProps {
   messages: IMessage[];
 }
 
-export class Messages extends Component<IProps> {
+interface IState {
+  newMessage: boolean;
+}
+
+export class Messages extends Component<IProps, IState> {
+  private list: HTMLElement | null;
+
+  private wasAtBottom: boolean;
+  state: IState = { newMessage: false };
+
+  get scrollBottom() {
+    return this.list ? this.list.scrollHeight - this.list.clientHeight : 0;
+  }
+
+  componentWillReceiveProps(nextProps: IProps): void {
+    this.wasAtBottom = this.isScrolledToBottom();
+  }
+
+  componentDidUpdate(prevProps: IProps): void {
+    if (this.props.messages !== prevProps.messages) {
+      if (this.wasAtBottom) {
+        this.scrollToBottom();
+      }
+      else {
+        this.setState({ newMessage: true });
+      }
+    }
+  }
+
+  isScrolledToBottom(): boolean {
+    if (this.list) {
+      if (this.list.scrollTop == this.scrollBottom) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  scrollToBottom(): void {
+    if (this.list) {
+      this.list.scrollTop = this.list.scrollHeight - this.list.clientHeight
+    }
+  }
+
   render(): JSX.Element | null {
     const messages = this.props.messages.map((message, idx) => {
       return <Message key={idx} message={message} />;
     });
 
-    return <ul className={styles.messages}>{messages}</ul>;
+    return <div className={styles.messages}>
+      <ul ref={el => this.list = el} className={styles.list}>{messages}</ul>
+      {this.state.newMessage && <div className={styles.newMessages}>You've got new MESSAGES!</div>}
+    </div>;
   }
 }
