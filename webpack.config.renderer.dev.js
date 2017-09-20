49a38e925a5a7eb4ce28a7e205dd36f5fa7f1e21
samuelmaddock <samuel.maddock@gmail.com>
2017-09-20 00:46:27 -0400

    Remove SVG font loader

--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -155,17 +155,6 @@ export default merge.smart(baseConfig, {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
         use: 'file-loader',
       },
-      // SVG Font
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            mimetype: 'image/svg+xml',
-          }
-        }
-      },
       // Common Image Formats
       {
         test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
