e29c41082b1dc8d9872e47153265407469e9db15
samuelmaddock <samuel.maddock@gmail.com>
2017-09-20 23:05:45 -0400

    Convert Root component to a class

--- app/containers/Root.tsx
+++ app/containers/Root.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Store } from 'redux';
 import { Provider } from 'react-redux';
 import { History } from 'history';
@@ -10,12 +10,15 @@ interface IProps {
   history: History;
 }
 
-export default function Root({ store, history }: IProps) {
-  return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Routes />
-      </ConnectedRouter>
-    </Provider>
-  );
+export default class Root extends Component<IProps> {
+  render() {
+    const { store, history } = this.props;
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
+      </Provider>
+    );
+  }
 }
