ef5aecac6db09292ae0cd5cf29f25f6960f5e987
samuelmaddock <samuel.maddock@gmail.com>
2017-10-08 22:17:01 -0400

    Add 'Googlebot' to the Mediabot User Agent string

--- app/constants/http.ts
+++ app/constants/http.ts
@@ -1,8 +1,10 @@
 /**
  * Use bot UA to fetch prerendered webpages.
  * Using Googlebot UA from a non-Google IP triggers Cloudflare's blocker.
+ *
+ * Twitch doesn't serve og:meta unless 'Googlebot' is in the UA :|
  */
 export const MEDIA_USER_AGENT =
-  'Mozilla/5.0 (compatible; Mediabot/2.1; +http://samuelmaddock.com/)';
+  'Mozilla/5.0 (compatible; Mediabot/1.0; Googlebot; +http://samuelmaddock.com/)';
 
 export const MEDIA_REFERRER = 'http://mediaplayer.samuelmaddock.com/';
