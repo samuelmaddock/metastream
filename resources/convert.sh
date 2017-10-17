c8486e59a37b524c0bc18b9175a646b4d690881c
samuelmaddock <samuel.maddock@gmail.com>
2017-10-16 21:19:50 -0400

    Fix windows ico

--- resources/convert.sh
+++ resources/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 convert -resize 256x256 -background none icon.svg icon.png
-convert -resize 256x256 -background none icon.svg icon.ico
-convert -resize 256x256 -background none icon.svg icon.icns
+convert -background none -colors 256 icon.png icon.ico
+convert -background none icon.png icon.icns
 
 for Size in {16,24,32,48,64,96,128,256,512,1024}
 do
