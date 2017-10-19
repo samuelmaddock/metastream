cf8e80c4d3668a17cb3d0145338acf5afa4e0561
samuelmaddock <samuel.maddock@gmail.com>
2017-10-19 01:10:39 -0400

    Improve timestamp pair parsing to support left or righthand label

--- app/utils/cuepoints.ts
+++ app/utils/cuepoints.ts
@@ -1,18 +1,31 @@
-const TIMESTAMP_REGEX = /(\d+:\d\d(?::\d\d)?)[^\d]([^\n]+)?/g;
+const TIMESTAMP_REGEX = /(\d+:\d\d(?::\d\d)?)/;
 
-export const parseTimestampPairs = (str: string) => {
-  const results = [];
+type TimestampPair = [/*time*/ string, /*label*/ string];
 
-  let match;
-  while ((match = TIMESTAMP_REGEX.exec(str))) {
-    const time = match[1];
-    const label = match[2];
-    if (time) {
-      results.push([time.trim(), (label || '').trim()]);
-    }
-  }
+/** Parse string for pairs of timestamp and label */
+export const parseTimestampPairs = (str: string): TimestampPair[] => {
+  const lines = str.split('\n');
 
-  return results;
+  const results = lines
+    .map(line => {
+      const match = TIMESTAMP_REGEX.exec(line);
+      if (match) {
+        const time = match[1];
+        const start = match.index;
+        const end = start + time.length;
+
+        // add/remove 1 char to skip wrapping
+        const lhs = line.substring(0, start - 1);
+        const rhs = line.substring(end + 1, line.length);
+
+        const label = lhs.length > rhs.length ? lhs : rhs;
+
+        return [time, label.trim()];
+      }
+    })
+    .filter(Boolean);
+
+  return results as TimestampPair[];
 };
 
 export const timestampToMilliseconds = (str: string) => {
