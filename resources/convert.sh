e303374e06a28a31ecbe891b1b4c2d62fe99c663
samuelmaddock <samuel.maddock@gmail.com>
2017-11-13 22:26:14 -0500

    Add macOS icns file

--- resources/convert.sh
+++ resources/convert.sh
@@ -8,3 +8,13 @@ for Size in {16,24,32,48,64,96,128,256,512,1024}
 do
 	eval "convert -resize ${Size}x${Size} -background none icon.svg icons/${Size}x${Size}.png"
 done
+
+# macOS iconset
+for Size in {16,32,128,256,512}
+do
+	let RetinaSize=Size*2
+	eval "convert -resize ${Size}x${Size} -background none icon.svg icon.iconset/icon_${Size}x${Size}.png"
+	eval "convert -resize ${RetinaSize}x${RetinaSize} -background none icon.svg icon.iconset/icon_${Size}x${Size}@2x.png"
+done
+
+iconutil -c icns icon.iconset
