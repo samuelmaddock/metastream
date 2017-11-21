24de676c820d26b61575a86e812798c07b199136
samuelmaddock <samuel.maddock@gmail.com>
2017-11-20 19:00:13 -0500

    Regenerate icons

--- resources/convert.sh
+++ resources/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 convert -resize 256x256 -background none icon.svg icon.png
-convert -background none -colors 256 icon.png icon.ico
+convert -background none icon.png icon.ico
 convert -background none icon.png icon.icns
 
 for Size in {16,24,32,48,64,96,128,256,512,1024}
