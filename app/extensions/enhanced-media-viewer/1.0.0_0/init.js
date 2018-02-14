12ecb39511f940a801da6b7c47d882fde385858a
samuelmaddock <samuel.maddock@gmail.com>
2018-02-14 01:25:56 -0500

    Enhance enhanced media viewer

--- app/extensions/enhanced-media-viewer/1.0.0_0/init.js
+++ app/extensions/enhanced-media-viewer/1.0.0_0/init.js
@@ -1,49 +1,63 @@
-const body = document.body;
+const body = document.body
 
 function enhanceVideo(video) {
-  video.loop = true;
-  video.controls = false;
-  video.style.minWidth = '100%';
-  video.style.minHeight = '100%';
+  Object.assign(video, {
+    loop: true,
+    controls: false
+  })
+
+  Object.assign(video.style, {
+    minWidth: '100%',
+    minHeight: '100%'
+  })
 }
 
 function enhanceImage(image) {
-  const { src } = image;
+  const { src } = image
 
-  // TODO: why doesn't objectFit work?
-  if (image.style.cursor !== 'zoom-in') {
-    if (image.width > image.height) {
-      image.style.minWidth = '100%';
-    } else {
-      image.style.minHeight = '100%';
-    }
-  }
+  // Assume extension is correct because we can't get the MIME type
+  const isGif = src.endsWith('gif')
 
-  let bg = document.createElement('div');
-  Object.assign(bg.style, {
-    backgroundImage: `url(${src})`,
-    backgroundSize: 'cover',
-    backgroundPosition: '50% 50%',
-    position: 'fixed',
-    top: 0,
-    left: 0,
+  Object.assign(image.style, {
     width: '100%',
     height: '100%',
-    zIndex: '-1',
-    filter: 'blur(20px) brightness(0.7)',
-    transform: 'scale(1.2)'
-  });
-  body.insertBefore(bg, document.body.firstChild);
+    objectFit: 'contain',
+    background: null,
+    cursor: null,
+    webkitUserDrag: 'none'
+  })
+
+  // Create new image which doesn't inherit any default zoom behavior
+  const img = image.cloneNode(true)
+  body.replaceChild(img, image)
+
+  if (!isGif) {
+    let bg = document.createElement('div')
+    Object.assign(bg.style, {
+      backgroundImage: `url(${src})`,
+      backgroundSize: 'cover',
+      backgroundPosition: '50% 50%',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      zIndex: '-1',
+      filter: 'blur(20px) brightness(0.66)',
+      transform: 'scale(1.2)'
+    })
+    body.insertBefore(bg, document.body.firstChild)
+  }
 }
 
 if (body && body.childElementCount === 1) {
-  const video = document.querySelector('body video[autoplay]');
+  const video = document.querySelector('body > video[autoplay]')
   if (video) {
-    enhanceVideo(video);
+    enhanceVideo(video)
   }
 
-  const image = document.querySelector('body img');
+  const image = document.querySelector('body > img')
   if (image) {
-    enhanceImage(image);
+    enhanceImage(image)
   }
 }
