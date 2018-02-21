44708bfba507cb5bbbe90baa669d832f8b26f1ab
samuelmaddock <samuel.maddock@gmail.com>
2018-02-21 02:42:44 -0500

    Cache username for faster initialization

--- app/renderer/lobby/middleware/users.ts
+++ app/renderer/lobby/middleware/users.ts
@@ -4,8 +4,8 @@ import { Platform } from 'renderer/platform/types'
 import { PlatformService } from 'renderer/platform'
 import { localUser, NetConnection, NetServer } from 'renderer/network'
 import { NetMiddlewareOptions, NetActions } from 'renderer/network/actions'
-import { RpcThunk } from '../types';
-import { rpc, RpcRealm } from '../../network/middleware/rpc';
+import { RpcThunk } from '../types'
+import { rpc, RpcRealm } from '../../network/middleware/rpc'
 
 interface IUserPayload {
   conn: NetConnection
@@ -16,13 +16,27 @@ export const addUser = actionCreator<IUserPayload>('ADD_USER')
 export const removeUser = actionCreator<string>('REMOVE_USER')
 export const clearUsers = actionCreator<string>('CLEAR_USERS')
 
-const sendUsername = (name: string): RpcThunk<void> => (dispatch, getState, context) => {
-  dispatch(addUser({
-    conn: context.client,
-    name: name
-  }));
-};
-export const server_sendUsername = rpc(RpcRealm.Server, sendUsername);
+const usernameCache = new Map<string, string>()
+
+const setUsername = (name: string): RpcThunk<void> => (dispatch, getState, context) => {
+  const id = context.client.id.toString()
+  const oldName = usernameCache.get(id)
+
+  // Skip update if already cached
+  if (oldName === name) {
+    return
+  }
+
+  dispatch(
+    addUser({
+      conn: context.client,
+      name
+    })
+  )
+
+  usernameCache.set(id, name)
+}
+const server_setUsername = rpc(RpcRealm.Server, setUsername)
 
 export const usersMiddleware = (): Middleware => {
   return <S extends Object>(store: MiddlewareAPI<S>) => {
@@ -44,7 +58,7 @@ export const usersMiddleware = (): Middleware => {
         )
 
         server.on('connect', (conn: NetConnection) => {
-          dispatch(addUser({ conn }))
+          dispatch(addUser({ conn, name: usernameCache.get(conn.id.toString()) }))
         })
 
         server.on('disconnect', (conn: NetConnection) => {
@@ -53,7 +67,7 @@ export const usersMiddleware = (): Middleware => {
       } else {
         server.once('connect', () => {
           const username = PlatformService.getUserName(localUser().id)
-          dispatch(server_sendUsername(username))
+          dispatch(server_setUsername(username))
         })
       }
     }
