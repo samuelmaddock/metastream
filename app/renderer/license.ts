a65d1b3eecee34b19136981a23f07219dc5c755e
samuelmaddock <samuel.maddock@gmail.com>
2018-02-24 18:28:45 -0500

    Add licensed route

---
+++ app/renderer/license.ts
@@ -0,0 +1,3 @@
+export function hasValidLicense() {
+  return false
+}
--- app/renderer/routes.tsx
+++ app/renderer/routes.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import { HomePage } from './containers/HomePage';
-import { ServerBrowserPage } from './containers/ServerBrowserPage';
-import { LobbyPage } from './containers/LobbyPage';
-import { SessionJoinPage } from './containers/SessionJoinPage';
+import React from 'react'
+import { Switch, Route, Redirect, RouteProps } from 'react-router'
+import App from './containers/App'
+import { HomePage } from './containers/HomePage'
+import { ServerBrowserPage } from './containers/ServerBrowserPage'
+import { LobbyPage } from './containers/LobbyPage'
+import { SessionJoinPage } from './containers/SessionJoinPage'
+import { hasValidLicense } from 'renderer/license'
 
 export default () => (
   <App>
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/lobby/:lobbyId" component={LobbyPage} />
-      <Route path="/servers" component={ServerBrowserPage} />
-      <Route path="/join" component={SessionJoinPage} />
+      <LicensedRoute exact path="/" component={HomePage} />
+      <LicensedRoute path="/lobby/:lobbyId" component={LobbyPage} />
+      <LicensedRoute path="/servers" component={ServerBrowserPage} />
+      <LicensedRoute path="/join" component={SessionJoinPage} />
     </Switch>
   </App>
-);
+)
+
+interface PrivateRouteProps extends RouteProps {
+  component: any
+}
+
+// prettier-ignore
+const LicensedRoute = ({ component: Component, ...rest }: PrivateRouteProps) => (
+  <Route
+    {...rest}
+    render={props =>
+      hasValidLicense() ? (
+        <Component {...props} />
+      ) : (
+        <div>TODO: license page</div>
+      )
+    }
+  />
+)
