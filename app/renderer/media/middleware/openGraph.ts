52f6ac84534ababf1a9fe8e57a47861fdca26087
samuelmaddock <samuel.maddock@gmail.com>
2018-02-25 01:57:51 -0500

    Prevent openGraph data defaulting to '0' media duration
    0 is reserved for livestreaming media

--- app/renderer/media/middleware/openGraph.ts
+++ app/renderer/media/middleware/openGraph.ts
@@ -1,58 +1,58 @@
-import { load } from 'cheerio';
-import { MediaThumbnailSize, IMediaMiddleware, IMediaResponse } from '../types';
-import { fetchText } from 'utils/http';
-import { Url } from 'url';
-import { MEDIA_USER_AGENT } from 'constants/http';
+import { load } from 'cheerio'
+import { MediaThumbnailSize, IMediaMiddleware, IMediaResponse } from '../types'
+import { fetchText } from 'utils/http'
+import { Url } from 'url'
+import { MEDIA_USER_AGENT } from 'constants/http'
 
-import { parse } from './og';
+import { parse } from './og'
 
 function buildHTMLMetadata(url: Url, body: string): Partial<IMediaResponse> {
-  const og = parse(body, {});
-  console.log('og', og);
-  const { ogTitle: title, ogImage: image } = og;
+  const og = parse(body, {})
+  console.log('og', og)
+  const { ogTitle: title, ogImage: image } = og
 
   const thumbnails = image
     ? {
         [MediaThumbnailSize.Default]: image
       }
-    : undefined;
+    : undefined
 
   const meta: Partial<IMediaResponse> = {
     url: url.href!,
     title,
     thumbnails
-  };
-
-  if (og.ogVideo) {
-    meta.url = og.ogVideo.url || meta.url;
-    meta.duration = og.ogVideo.duration || 0;
   }
 
-  return meta;
+  if (og.ogVideo) {
+    meta.url = og.ogVideo.url || meta.url
+    meta.duration = og.ogVideo.duration
+  }
+
+  return meta
 }
 
 const mware: IMediaMiddleware = {
   match({ protocol }) {
-    return protocol === 'http:' || protocol === 'https:';
+    return protocol === 'http:' || protocol === 'https:'
   },
 
   async resolve(ctx, next) {
-    const { url } = ctx.req;
+    const { url } = ctx.req
 
     const [text, response] = await fetchText(url.href, {
       headers: {
         'user-agent': MEDIA_USER_AGENT
       }
-    });
+    })
 
-    ctx.state.body = text;
-    ctx.state.$ = load(text);
+    ctx.state.body = text
+    ctx.state.$ = load(text)
 
-    const meta = buildHTMLMetadata(url, text);
-    Object.assign(ctx.res, meta);
+    const meta = buildHTMLMetadata(url, text)
+    Object.assign(ctx.res, meta)
 
-    return next();
+    return next()
   }
-};
+}
 
-export default mware;
+export default mware
