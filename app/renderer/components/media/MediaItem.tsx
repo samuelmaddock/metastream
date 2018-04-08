d54d29a3fac73dae2c085323ac27ec6d9aacee95
samuelmaddock <samuel.maddock@gmail.com>
2018-04-08 01:11:47 -0400

    Fix duration being shown for live media

--- app/renderer/components/media/MediaItem.tsx
+++ app/renderer/components/media/MediaItem.tsx
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import { IMediaItem } from 'renderer/lobby/reducers/mediaPlayer';
-import styles from './Media.css';
-import { formatMs } from 'utils/time';
-import { isNumber } from 'utils/type';
+import React, { Component } from 'react'
+import { IMediaItem } from 'renderer/lobby/reducers/mediaPlayer'
+import styles from './Media.css'
+import { formatMs } from 'utils/time'
+import { isNumber } from 'utils/type'
 
 interface IProps {
-  media: IMediaItem;
+  media: IMediaItem
 }
 
 export class MediaItem extends Component<IProps> {
   render(): JSX.Element | null {
-    const { media } = this.props;
+    const { media } = this.props
 
     return (
       <figure className={styles.media}>
         <div className={styles.title}>{media.title}</div>
-        {isNumber(media.duration) && (
-          <span className={styles.duration}>{formatMs(media.duration)}</span>
-        )}
+        {isNumber(media.duration) &&
+          media.duration !== 0 && (
+            <span className={styles.duration}>{formatMs(media.duration)}</span>
+          )}
         {media.ownerName && (
           <div>
             <span className={styles.authorLabel}>Added by</span>
@@ -25,6 +26,6 @@ export class MediaItem extends Component<IProps> {
           </div>
         )}
       </figure>
-    );
+    )
   }
 }
