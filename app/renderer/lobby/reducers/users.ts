6ecae1072d80b4b9b250227fb03c69d5b41c9d23
samuelmaddock <samuel.maddock@gmail.com>
2018-05-15 00:39:59 -0400

    Move user helpers into separate file

--- app/renderer/lobby/reducers/users.ts
+++ app/renderer/lobby/reducers/users.ts
@@ -2,8 +2,6 @@ import { Reducer } from 'redux'
 import { isType } from 'utils/redux'
 import { IAppState } from 'renderer/reducers'
 import { addUser, removeUser, clearUsers } from '../middleware/users'
-import { localUserId } from 'renderer/network'
-import { DEFAULT_USERNAME, DEFAULT_COLOR } from '../../../constants/settings'
 
 export interface IUser {
   id: string
@@ -63,38 +61,4 @@ export const users: Reducer<IUsersState> = (state: IUsersState = initialState, a
   }
 
   return state
-}
-
-export const getUser = (state: IAppState, userId: string) => state.users.map[userId]
-
-export const getUserName = (state: IAppState, userId: string): string => {
-  const user = getUser(state, userId)
-  return user ? user.name : DEFAULT_USERNAME
-}
-
-export const getUserColor = (state: IAppState, userId: string): string => {
-  const user = getUser(state, userId)
-  return user ? user.color : DEFAULT_COLOR
-}
-
-export const getHostId = (state: IAppState) => state.users.host
-export const getHost = (state: IAppState) => getUser(state, getHostId(state))!
-export const isHost = (state: IAppState, userId: string = localUserId()) =>
-  getHostId(state) === userId
-
-export const isAdmin = (state: IAppState, userId: string = localUserId()) => {
-  const user = getUser(state, userId)
-  return Boolean(user && user.admin)
-}
-
-export const getNumUsers = (state: IAppState) => Object.keys(state.users.map).length
-
-export const findUser = (state: IAppState, filter: (user: IUser, index: number) => boolean) => {
-  const userIds = Object.keys(state.users.map)
-  for (let i = 0; i < userIds.length; i++) {
-    const user = state.users.map[userIds[i]]!
-    if (filter(user, i)) {
-      return user
-    }
-  }
 }
