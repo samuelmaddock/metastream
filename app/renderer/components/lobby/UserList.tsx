f7e38b31aa0970222d1b9940cfdedf946849aae4
samuelmaddock <samuel.maddock@gmail.com>
2018-06-27 00:41:49 -0400

    Add initial user session mode button

--- app/renderer/components/lobby/UserList.tsx
+++ app/renderer/components/lobby/UserList.tsx
@@ -25,37 +25,59 @@ interface IConnectedProps {
 type Props = IProps & IConnectedProps
 
 class _UserList extends Component<Props> {
+  private get userIds() {
+    return Object.keys(this.props.users.map)
+  }
+
+  private get numUsers() {
+    return this.userIds.length
+  }
+
+  private get userSlots() {
+    const { numUsers } = this
+    const { maxUsers } = this.props
+    return `${numUsers}` + (maxUsers && isFinite(maxUsers) ? `/${maxUsers}` : '')
+  }
+
   render(): JSX.Element | null {
-    const { users, maxUsers } = this.props
-    const userIds = Object.keys(users.map)
-
-    const numUsers = userIds.length
-    const userSlots = `${numUsers}` + (maxUsers && isFinite(maxUsers) ? `/${maxUsers}` : '')
-
     return (
       <ListOverlay
         className={this.props.className}
         title={t('users')}
-        tagline={userSlots}
-        action={
-          <HighlightButton icon="mail" highlight={numUsers < 2} onClick={this.props.onInvite}>
-            {t('invite')}
-          </HighlightButton>
-        }
+        tagline={this.userSlots}
+        action={this.renderActions()}
       >
-        {userIds.map((userId: string) => {
-          const user = users.map[userId]!
+        {this.userIds.map((userId: string) => {
+          const user = this.props.users.map[userId]!
           return <UserItem key={userId} user={user} />
         })}
       </ListOverlay>
     )
   }
+
+  renderActions() {
+    return (
+      <>
+        {this.props.isHost && (
+          <HighlightButton
+            icon={(this.props.maxUsers || 0) > 1 ? 'users' : 'user'}
+            onClick={this.props.onInvite}
+          />
+        )}
+        <HighlightButton icon="mail" highlight={this.numUsers < 2} onClick={this.props.onInvite}>
+          {t('invite')}
+        </HighlightButton>
+      </>
+    )
+  }
 }
 
-export const UserList = connect((state: IAppState): IConnectedProps => {
-  return {
-    maxUsers: getMaxUsers(state),
-    users: state.users,
-    isHost: isHost(state)
+export const UserList = connect(
+  (state: IAppState): IConnectedProps => {
+    return {
+      maxUsers: getMaxUsers(state),
+      users: state.users,
+      isHost: isHost(state)
+    }
   }
-})(_UserList) as React.ComponentClass<IProps>
+)(_UserList) as React.ComponentClass<IProps>
