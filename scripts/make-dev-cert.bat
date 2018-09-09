75d6584a079b4ba131e0e84e32e4b4e475d6ace5
samuelmaddock <samuel.maddock@gmail.com>
2018-09-09 19:38:57 -0400

    Remove unused dev scripts

--- scripts/make-dev-cert.bat
+++
@@ -1,2 +0,0 @@
-makecert -sv metastream-dev.pvk -n "CN=MetastreamDev" metastream-dev.cer -b 05/08/2018 -e 11/01/2018 -r
-pvk2pfx -pvk metastream-dev.pvk -spc metastream-dev.cer -pfx metastream-dev.pfx -po password
--- scripts/package-dev-signed.sh
+++
@@ -1,1 +0,0 @@
-WIN_CSC_LINK=C:\\Users\\Sam\\source\\metastream-dev.pfx WIN_CSC_KEY_PASSWORD=password yarn package
