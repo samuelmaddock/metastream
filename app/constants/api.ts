a25f754e7e25fa46ea640b03b3974d8acf21b135
samuelmaddock <samuel.maddock@gmail.com>
2018-09-18 21:35:49 -0400

    Remove unused API constants

--- app/constants/api.ts
+++
@@ -1,8 +0,0 @@
-export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://getmetastream.com/api'
-    : 'http://localhost:5000/getmetastream/us-central1/api'
-
-export const CDN_URL = 'http://cdn.getmetastream.com/'
-
-export const API_ORIGIN = 'https://app.getmetastream.com/'
--- app/constants/updater.ts
+++ app/constants/updater.ts
@@ -1,5 +1,3 @@
-import { CDN_URL } from './api'
-
 export default (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true'
   ? {
       provider: 'generic',
