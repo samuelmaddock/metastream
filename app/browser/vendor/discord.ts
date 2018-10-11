83202eb9bbb05c82e889cc2dd742e940678b48b0
samuelmaddock <samuel.maddock@gmail.com>
2018-10-11 19:22:58 -0400

    Subscribe to Discord game join activities

--- app/browser/vendor/discord.ts
+++ app/browser/vendor/discord.ts
@@ -2,34 +2,45 @@ import { ipcMain } from 'electron'
 import { throttle } from 'lodash'
 import * as DiscordRPC from 'discord-rpc'
 import log from '../log'
+import { getMainWindow } from '../window'
 
 let discordRpc: any | null = null
-let initialized = false
+let activityCache: DiscordActivity | null = null
 
-const init = () => {
-  return new Promise((resolve, reject) => {
-    const clientId = process.env.DISCORD_CLIENT_ID
-    if (!clientId) {
-      throw new Error('Discord Client ID not configured.')
+const init = async () => {
+  const clientId = process.env.DISCORD_CLIENT_ID
+  if (!clientId) {
+    throw new Error('Discord Client ID not configured.')
+  }
+
+  DiscordRPC.register(clientId)
+
+  const rpc = new DiscordRPC.Client({ transport: 'ipc' })
+  discordRpc = rpc
+
+  rpc.once('ready', () => {
+    const send = (eventName: string, ...args: any[]) => {
+      const win = getMainWindow()
+      if (win) {
+        win.webContents.send(eventName, ...args)
+      }
     }
 
-    DiscordRPC.register(clientId)
+    rpc.subscribe('ACTIVITY_JOIN', ({ secret }: any) => send('discord-join', secret))
+    rpc.subscribe('ACTIVITY_JOIN_REQUEST', (user: any) => send('discord-join-request', user))
 
-    const rpc = new DiscordRPC.Client({ transport: 'ipc' })
-    discordRpc = rpc
-
-    rpc.once('ready', () => {
-      initialized = true
-      resolve()
-    })
-
-    try {
-      rpc.login({ clientId })
-    } catch (e) {
-      log.error(e)
-      return
+    if (activityCache) {
+      updateActivity(activityCache)
+      activityCache = null
     }
   })
+
+  try {
+    await rpc.login({ clientId })
+  } catch (e) {
+    log.error(e)
+    return
+  }
 }
 
 const updateActivity = throttle(async (activity: DiscordActivity) => {
@@ -51,5 +62,36 @@ ipcMain.on('set-discord-enabled', (event: Electron.Event, enabled: boolean) => {
 })
 
 ipcMain.on('set-discord-activity', (event: Electron.Event, activity: DiscordActivity) => {
-  updateActivity(activity)
+  if (discordRpc) {
+    updateActivity(activity)
+  } else {
+    activityCache = activity
+  }
+})
+
+ipcMain.on('send-discord-reply', async (event: Electron.Event, user: any, response: string) => {
+  if (!discordRpc) return
+
+  let responsePromise
+
+  switch (response) {
+    case 'YES':
+      responsePromise = discordRpc.sendJoinInvite(user)
+      break
+    case 'NO':
+    case 'IGNORE':
+      responsePromise = discordRpc.closeJoinRequest(user)
+      break
+    default:
+      log.error('Unknown Discord response')
+      return
+  }
+
+  if (responsePromise) {
+    try {
+      await responsePromise
+    } catch (e) {
+      log.error(e)
+    }
+  }
 })
