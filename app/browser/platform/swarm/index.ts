2ee50a76f75f428bf4fe7b3c0bbe39616965eb27
samuelmaddock <samuel.maddock@gmail.com>
2018-11-12 22:25:10 -0500

    Delete swarm client upon destroying webcontents

--- app/browser/platform/swarm/index.ts
+++ app/browser/platform/swarm/index.ts
@@ -19,11 +19,16 @@ const swarmClients = new Map<number, SwarmClient>()
 const getSwarmClient = (event: Electron.Event) => swarmClients.get(event.sender.id)
 
 ipcMain.on('platform-swarm-init', async (event: Electron.Event) => {
-  const client = new SwarmClient(event.sender)
-  swarmClients.set(event.sender.id, client)
+  const wc = event.sender
+  const wcId = wc.id
 
-  const isMainWindow = event.sender.id === 1
+  const client = new SwarmClient(wc)
+  swarmClients.set(wcId, client)
+  wc.once('destroyed', () => swarmClients.delete(wcId))
+
+  const isMainWindow = wcId === 1
   const identity = await client.initIdentity(isMainWindow)
+
   event.returnValue = identity
 })
 
