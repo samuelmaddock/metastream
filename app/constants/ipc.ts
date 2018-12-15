500594dff30763d75e17423809a4482ce30d3870
samuelmaddock <samuel.maddock@gmail.com>
2018-12-15 18:40:09 -0500

    Set app language on startup

---
+++ app/constants/ipc.ts
@@ -0,0 +1,2 @@
+export const REQUEST_LANGUAGE = 'request-language'
+export const LANGUAGE = 'language'
--- app/locale/index.ts
+++ app/locale/index.ts
@@ -4,6 +4,9 @@ import { reactI18nextModule } from 'react-i18next'
 import enUS from './en-US'
 import deDE from './de_DE'
 import ptBR from './pt-BR'
+import { REQUEST_LANGUAGE, LANGUAGE } from 'constants/ipc'
+
+export const DEFAULT_LANGUAGE = 'en-US'
 
 const resources = {
   'de-DE': {
@@ -20,8 +23,8 @@ const resources = {
 i18n.use(reactI18nextModule).init({
   debug: process.env.NODE_ENV === 'development',
   resources,
-  lng: 'en-US',
-  fallbackLng: 'en-US',
+  lng: DEFAULT_LANGUAGE,
+  fallbackLng: DEFAULT_LANGUAGE,
   keySeparator: false, // we do not use keys in form messages.welcome
   interpolation: {
     escapeValue: false // react already safes from xss
@@ -38,4 +41,19 @@ export const translateEscaped: typeof t = (key, vars) => {
       escapeValue: true
     }
   })
+}
+
+export function initLocale() {
+  const { ipcRenderer } = chrome
+  ipcRenderer.on(LANGUAGE, (e: Electron.Event, lang: string) => {
+    console.debug(`Setting language to ${lang}`)
+    if (lang !== i18n.language) {
+      i18n.changeLanguage(lang)
+    }
+  })
+  ipcRenderer.send(REQUEST_LANGUAGE)
+
+  if (process.env.NODE_ENV === 'development') {
+    Object.assign((window as any).app, { i18n })
+  }
 }
