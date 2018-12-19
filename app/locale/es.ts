d21a1cf8c122ed9525d3dad544bf7219947502c7
samuelmaddock <samuel.maddock@gmail.com>
2018-12-18 21:48:39 -0500

    Register Spanish language

--- app/locale/es.ts
+++ app/locale/es.ts
@@ -35,7 +35,8 @@ export default {
   enableDiscordInvite:
     'Active <1>Discord Rich Presence</1> desde el menú de configuración para permitir invitaciones de Discord.',
   endSessionTitle: '¿Finalizar sesión pública?',
-  endSessionDescription: 'Cambiar a una sesión privada desconectará a todos los usuarios conectados.',
+  endSessionDescription:
+    'Cambiar a una sesión privada desconectará a todos los usuarios conectados.',
   enterJoinDest: 'Ingrese un código de amigo o dirección IP.',
   extensions: 'Extensiones',
   friendCode: 'Código de amigo',
@@ -45,7 +46,8 @@ export default {
   join: 'Unirse',
   joinSession: 'Unirse a la sesión',
   kick: 'Expulsar',
-  launchDiscordInvite: 'Habra Discord y reinicie {{productName}} para usar invitaciones de Discord.',
+  launchDiscordInvite:
+    'Habra Discord y reinicie {{productName}} para usar invitaciones de Discord.',
   lockQueue: 'Bloquear cola',
   moveToTop: 'Mover al principio',
   next: 'Siguiente',
@@ -56,7 +58,8 @@ export default {
   networkDisconnectVersionMismatch: `Versión de cliente diferente.`,
   networkDisconnectFull: 'La sesión está llena.',
   networkDisconnectKicked: 'Expulsado de la sesión.',
-  networkTroubleshootingHelp: 'Coteja la <1>Guía para diagnosticar la red</1> para conseguir ayuda.',
+  networkTroubleshootingHelp:
+    'Coteja la <1>Guía para diagnosticar la red</1> para conseguir ayuda.',
   noticeAddedMedia: '<Media id="{{mediaId}}">{{mediaTitle}}</Media> ha sido añadido',
   noticeMediaError: 'Hubo un error solicitando {{url}}',
   noticeNowPlaying:
@@ -74,25 +77,27 @@ export default {
   repeat: 'Repetir',
   requestUrl: 'Añadir a sesión',
   requestUrlPlaceholder: '',
-  requiresDJPermissions: 'Requiere permisos de DJ'',
+  requiresDJPermissions: 'Requiere permisos de DJ',
   requiresPortForward:
     'Para aceptar conexiones directas se requiere <1>enrutar los siguientes puertos</1> <3>{{port}}</3>.',
-  sendDiscordInvite: '<1>Enviar invitaciones de Discord</1> para compartir código de amigo automáticamente.',
+  sendDiscordInvite:
+    '<1>Enviar invitaciones de Discord</1> para compartir código de amigo automáticamente.',
   sessionModePublicDescription: 'Cualquier usuario puede unirse.',
   sessionModePrivateDescription: 'Permiso para unirse debe ser otorgado explicitamente.',
   sessionModeOfflineDescription: 'Nadie está permitido unirse.',
   settings: 'Ajustes',
   shareFriendCode: 'Comparte el siguiente código de amigo para invitar amigos.',
   shareFriendCodeOther: 'Comparte el siguiente código de amigo de {{name}} para invitar amigos.',
-  sharePublicIP: 'Comparta <1>su IP público</1> para permitir que sus amigos se conecten directamente.',
+  sharePublicIP:
+    'Comparta <1>su IP público</1> para permitir que sus amigos se conecten directamente.',
   startSession: 'Comenzar Sesión',
   thirdPartyIntegrations: 'Integraciones externas',
-  toggleDJ: 'Activar/Desactivar DJ'',
+  toggleDJ: 'Activar/Desactivar DJ',
   unlockQueue: 'Desbloquear cola',
   username: 'Usuario',
   users: 'Usuarios',
   userJoined: '<Username id="{{userId}}">{{username}}</Username> se ha unido.',
-  userLeft: '<Username id="{{userId}}">{{username}}</Username> se ha ido.'.',
+  userLeft: '<Username id="{{userId}}">{{username}}</Username> se ha ido.',
   viewOnGitHub: 'Ver en GitHub',
   waitingForHost: 'Esperando que el servidor permita la conexión.',
   welcome: 'Bienvenido'
