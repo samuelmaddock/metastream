5104aa8d34ed0e6a1c1f77575997b1ec408d6a28
samuelmaddock <samuel.maddock@gmail.com>
2019-04-14 23:43:05 -0400

    Add media enhancement script

--- packages/media-remote-extension/player.js
+++ packages/media-remote-extension/player.js
@@ -9,6 +9,10 @@
 
 console.debug(`Metastream player content script ${location.href}`)
 
+//=============================================================================
+// Setup communications between content script and background script.
+//=============================================================================
+
 // Listen for events from the main world to forward to the
 // background process
 const eventMiddleware = event => {
@@ -30,9 +34,80 @@ chrome.runtime.onMessage.addListener(message => {
   }
 })
 
+//=============================================================================
+// Improve visuals of image or video pages
+//=============================================================================
+
+const body = document.body
+
+function enhanceVideo(video) {
+  Object.assign(video, {
+    loop: false,
+    controls: false
+  })
+
+  Object.assign(video.style, {
+    minWidth: '100%',
+    minHeight: '100%'
+  })
+}
+
+function enhanceImage(image) {
+  const { src } = image
+
+  // Assume extension is correct because we can't get the MIME type
+  const isGif = src.endsWith('gif')
+
+  Object.assign(image.style, {
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+    background: null,
+    cursor: null,
+    webkitUserDrag: 'none'
+  })
+
+  // Create new image which doesn't inherit any default zoom behavior
+  const img = image.cloneNode(true)
+  body.replaceChild(img, image)
+
+  if (!isGif) {
+    let bg = document.createElement('div')
+    Object.assign(bg.style, {
+      backgroundImage: `url(${src})`,
+      backgroundSize: 'cover',
+      backgroundPosition: '50% 50%',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      zIndex: '-1',
+      filter: 'blur(20px) brightness(0.66)',
+      transform: 'scale(1.2)'
+    })
+    body.insertBefore(bg, body.firstChild)
+  }
+}
+
+if (body && body.childElementCount === 1) {
+  const video = document.querySelector('body > video[autoplay]')
+  if (video) {
+    enhanceVideo(video)
+  }
+
+  const image = document.querySelector('body > img')
+  if (image) {
+    enhanceImage(image)
+  }
+}
+
+//=============================================================================
+// Main world script - modifies media in the main browser context.
+//=============================================================================
+
 // Code within function will be injected into main world.
 // No closure variables are allowed within the function body.
-// TODO: compile this from separate file
 const mainWorldScript = function() {
   // Injected by Metastream
   console.debug(`Metastream main world script ${location.href}`)
