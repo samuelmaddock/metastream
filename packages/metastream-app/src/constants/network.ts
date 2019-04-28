6a913d5afffbc3e320c21bc15cb96d1983c9953a
samuelmaddock <samuel.maddock@gmail.com>
2019-04-28 16:18:51 -0400

    Use TCP transport for TURN relay

--- packages/metastream-app/src/constants/network.ts
+++ packages/metastream-app/src/constants/network.ts
@@ -8,11 +8,16 @@ const METASTREAM_STUN_SERVERS = [
   { url: 'stun:stun4.l.google.com:19302' }
 ]
 const METASTREAM_TURN_SERVER = {
-  url: process.env.METASTREAM_TURN_SERVER || 'turn:turn.rtc.getmetastream.com:5349',
+  url: process.env.METASTREAM_TURN_SERVER || 'turn:turn.rtc.getmetastream.com:5349?transport=tcp',
   username: process.env.METASTREAM_TURN_USERNAME || 'metastream',
   credential: process.env.METASTREAM_TURN_CREDENTIAL
 }
-export const METASTREAM_ICE_SERVERS = [...METASTREAM_STUN_SERVERS, METASTREAM_TURN_SERVER]
+
+// prettier-ignore
+export const METASTREAM_ICE_SERVERS = [
+  ...METASTREAM_STUN_SERVERS,
+  METASTREAM_TURN_SERVER
+]
 
 export const NETWORK_TIMEOUT = 15000
 export const RECONNECT_TIMEOUT = 30000
