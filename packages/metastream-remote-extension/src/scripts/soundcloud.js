d51e8043daade381763d68b4ad615f08bdc3c57c
samuelmaddock <samuel.maddock@gmail.com>
2019-10-22 22:34:19 -0400

    Remove soundcloud script
    This isn't needed anymore thanks to the MediaSession API. :)

--- packages/metastream-remote-extension/src/scripts/soundcloud.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('metastreamseek', e => {
-  e.preventDefault()
-  const time = e.detail
-  const action = { method: 'seekTo', value: time }
-  const json = JSON.stringify(action)
-  postMessage(json, location.origin)
-})
