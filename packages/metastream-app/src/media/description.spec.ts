9710abbc69365d9150de5647e401e39bbb639057
samuelmaddock <samuel.maddock@gmail.com>
2019-10-25 23:51:56 -0400

    Add bold text syntax parsing

--- packages/metastream-app/src/media/description.spec.ts
+++ packages/metastream-app/src/media/description.spec.ts
@@ -1,4 +1,12 @@
-import { parseDescription, TIMESTAMP_REGEX, LINK_REGEX, ParseToken } from './description'
+import {
+  parseDescription,
+  TIMESTAMP_REGEX,
+  LINK_REGEX,
+  ParseToken,
+  BOLD_REGEX,
+  ITALIC_REGEX,
+  parseText
+} from './description'
 
 it('matches timestamps', () => {
   expect(TIMESTAMP_REGEX.test('0:12')).toBeTruthy()
@@ -21,6 +29,68 @@ it('matches URLs', () => {
   expect(LINK_REGEX.test('http://')).toBeFalsy()
   expect(LINK_REGEX.test('ftp://unsupported-protocol')).toBeFalsy()
   expect(LINK_REGEX.test('not URL')).toBeFalsy()
+})
+
+it('matches bold text', () => {
+  expect(BOLD_REGEX.test('__bold__')).toBeTruthy()
+  expect(BOLD_REGEX.test('__bold__ ')).toBeTruthy()
+  expect(BOLD_REGEX.test(' __bold__')).toBeTruthy()
+  expect(BOLD_REGEX.test(' __bold__ ')).toBeTruthy()
+  expect(BOLD_REGEX.test('__not__bold')).toBeFalsy()
+  expect(BOLD_REGEX.test('_____')).toBeTruthy()
+  expect(BOLD_REGEX.test('__ _ __')).toBeTruthy()
+
+  expect(BOLD_REGEX.test('**bold**')).toBeTruthy()
+  expect(BOLD_REGEX.test('**bold** ')).toBeTruthy()
+  expect(BOLD_REGEX.test(' **bold**')).toBeTruthy()
+  expect(BOLD_REGEX.test(' **bold** ')).toBeTruthy()
+  expect(BOLD_REGEX.test('**not**bold')).toBeFalsy()
+  expect(BOLD_REGEX.test('*****')).toBeTruthy()
+  expect(BOLD_REGEX.test('** ** **')).toBeTruthy()
+})
+
+it('matches italic text', () => {
+  expect(ITALIC_REGEX.test('_italic_')).toBeTruthy()
+  expect(ITALIC_REGEX.test(' _italic_')).toBeTruthy()
+  expect(ITALIC_REGEX.test('_italic_ ')).toBeTruthy()
+  expect(ITALIC_REGEX.test(' _italic_ ')).toBeTruthy()
+
+  expect(ITALIC_REGEX.test('_not_italic')).toBeFalsy()
+  expect(ITALIC_REGEX.test('not_italic_')).toBeFalsy()
+})
+
+it('parses invalid timestamps as text', () => {
+  const parseTimestamp = (text: string) => parseText(text, [ParseToken.TIMESTAMP])
+  expect(parseTimestamp('1:23:45:67')).toEqual([{ type: ParseToken.TEXT, content: '1:23:45:67' }])
+  expect(parseTimestamp('1:22:33:44 11:22 1:22:33')).toEqual([
+    { type: ParseToken.TEXT, content: '1:22:33:44 ' },
+    { type: ParseToken.TIMESTAMP, content: '11:22' },
+    { type: ParseToken.TEXT, content: ' ' },
+    { type: ParseToken.TIMESTAMP, content: '1:22:33' }
+  ])
+  expect(parseTimestamp('test1:23')).toEqual([{ type: ParseToken.TEXT, content: 'test1:23' }])
+  expect(parseTimestamp('1:23test')).toEqual([{ type: ParseToken.TEXT, content: '1:23test' }])
+  expect(parseTimestamp('test1:23test')).toEqual([
+    { type: ParseToken.TEXT, content: 'test1:23test' }
+  ])
+})
+
+it('parses bold text', () => {
+  const parseBold = (text: string) => parseText(text, [ParseToken.BOLD])
+  expect(parseBold('__bold__')).toEqual([{ type: ParseToken.BOLD, content: 'bold' }])
+  expect(parseBold('yo __bold__ text')).toEqual([
+    { type: ParseToken.TEXT, content: 'yo ' },
+    { type: ParseToken.BOLD, content: 'bold' },
+    { type: ParseToken.TEXT, content: ' text' }
+  ])
+  expect(parseBold('__double__ __bold__')).toEqual([
+    { type: ParseToken.BOLD, content: 'double' },
+    { type: ParseToken.TEXT, content: ' ' },
+    { type: ParseToken.BOLD, content: 'bold' }
+  ])
+  expect(parseBold('__bold with text inside__')).toEqual([
+    { type: ParseToken.BOLD, content: 'bold with text inside' }
+  ])
 })
 
 it('parses description', () => {
@@ -48,20 +118,5 @@ Fin`
     },
     { type: ParseToken.LINK, content: 'https://soundcloud.com/user' },
     { type: ParseToken.TEXT, content: '\n\nFin' }
-  ])
-})
-
-it('ignores invalid timestamps', () => {
-  expect(parseDescription('1:23:45:67')).toEqual([{ type: ParseToken.TEXT, content: '1:23:45:67' }])
-  expect(parseDescription('1:22:33:44 11:22 1:22:33')).toEqual([
-    { type: ParseToken.TEXT, content: '1:22:33:44 ' },
-    { type: ParseToken.TIMESTAMP, content: '11:22' },
-    { type: ParseToken.TEXT, content: ' ' },
-    { type: ParseToken.TIMESTAMP, content: '1:22:33' }
-  ])
-  expect(parseDescription('test1:23')).toEqual([{ type: ParseToken.TEXT, content: 'test1:23' }])
-  expect(parseDescription('1:23test')).toEqual([{ type: ParseToken.TEXT, content: '1:23test' }])
-  expect(parseDescription('test1:23test')).toEqual([
-    { type: ParseToken.TEXT, content: 'test1:23test' }
   ])
 })
--- packages/metastream-app/src/media/description.ts
+++ packages/metastream-app/src/media/description.ts
@@ -1,25 +1,47 @@
-// timestamp pattern regex
 export const TIMESTAMP_REGEX = /\b(\d{1,2})((?::\d{2})+)\b/m
-
-// simple URL regex
 export const LINK_REGEX = /\b(https?:\/\/\S+)\b/m
+export const BOLD_REGEX = /(?:^|\s)(?:(?:\*{2}(.+?)\*{2})|(?:_{2}(.+?)_{2}))(?:$|\s)/m
+export const ITALIC_REGEX = /(?:^|\s)(?:(?:\*(.+?)\*)|(?:_(.+?)_))(?:$|\s)/m
 
 export const enum ParseToken {
   TEXT = 'text',
   TIMESTAMP = 'timestamp',
-  LINK = 'link'
+  LINK = 'link',
+  BOLD = 'bold',
+  ITALIC = 'italic'
 }
 
 export type DescriptionFragment = { type: ParseToken; content: string }
 
+type TokenizeResult = {
+  startIndex: number
+  endIndex?: number
+  value: string
+}
+
 type TokenConfig = {
-  token: ParseToken
-  tokenize: (text: string) => [number, string] | void
+  type: ParseToken
+  tokenize: (text: string) => TokenizeResult | void
 }
 
 const tokenConfigs: TokenConfig[] = [
   {
-    token: ParseToken.TIMESTAMP,
+    type: ParseToken.BOLD,
+    tokenize: (text: string) => {
+      let startIndex = text.search(BOLD_REGEX)
+      if (startIndex === -1) return
+      const match = text.match(BOLD_REGEX)!
+      const full = match[0]
+      const value = match[1] || match[2]
+      const prefixChars = full.startsWith('_') ? 0 : 1
+      const postfixChars = full.endsWith('_') ? 0 : 1
+      const endIndex = startIndex + full.length - postfixChars
+      startIndex += prefixChars
+      return { startIndex, endIndex, value }
+    }
+  },
+  {
+    type: ParseToken.TIMESTAMP,
     tokenize: text => {
       let subtext = text
       let pos = 0
@@ -38,26 +60,27 @@ const tokenConfigs: TokenConfig[] = [
           continue
         }
 
-        return [pos + startIndex, timestamp]
+        return { startIndex: pos + startIndex, value: timestamp }
 
         // continue search through remaining string
       } while (pos <= text.length)
     }
   },
   {
-    token: ParseToken.LINK,
+    type: ParseToken.LINK,
     tokenize: (text: string) => {
       const startIndex = text.search(LINK_REGEX)
       if (startIndex === -1) return
-      const link = text.match(LINK_REGEX)![0]
-      return [startIndex, link]
+      const value = text.match(LINK_REGEX)![0]
+      return { startIndex, value }
     }
   }
 ]
 
-const lexParts = (parts: DescriptionFragment[]) => {
+const lexParts = (parts: DescriptionFragment[], tokens: ParseToken[]) => {
   // take multiple tokenization passes given the number of tokens
-  tokenConfigs.forEach(config => {
+  const configs = tokenConfigs.filter(cfg => tokens.includes(cfg.type))
+  configs.forEach(config => {
     for (let i = 0; i < parts.length; i++) {
       const { type, content } = parts[i]
       if (type !== ParseToken.TEXT) continue
@@ -65,11 +88,11 @@ const lexParts = (parts: DescriptionFragment[]) => {
       const result = config.tokenize(content)
       if (!result) continue
 
-      const [startIndex, value] = result
-      const endIndex = startIndex + value.length
+      const { startIndex, value } = result
+      const endIndex = result.endIndex ? result.endIndex : startIndex + value.length
 
       // overwrite current index with value
-      parts[i] = { type: config.token, content: value }
+      parts[i] = { type: config.type, content: value }
 
       // insert remaining text after current index
       if (endIndex < content.length) {
@@ -88,8 +111,14 @@ const lexParts = (parts: DescriptionFragment[]) => {
   return parts
 }
 
-export const parseDescription = (text: string) => {
+export const parseText = (text: string, tokens: ParseToken[]) => {
   let parts = [{ type: ParseToken.TEXT, content: text }]
-  parts = lexParts(parts)
+  parts = lexParts(parts, tokens)
   return parts
 }
+
+export const parseDescription = (text: string) =>
+  parseText(text, [ParseToken.TIMESTAMP, ParseToken.LINK])
+
+export const parseChatMessage = (text: string) =>
+  parseText(text, [ParseToken.BOLD, ParseToken.TIMESTAMP, ParseToken.LINK])
