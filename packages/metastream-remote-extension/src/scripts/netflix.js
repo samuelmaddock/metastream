31082c60f48be4a6233f304b89d5f1845790458c
samuelmaddock <samuel.maddock@gmail.com>
2019-11-18 17:26:04 -0500

    Update netflix script to be more pure

--- packages/metastream-remote-extension/src/scripts/netflix.js
+++ packages/metastream-remote-extension/src/scripts/netflix.js
@@ -1,55 +1,38 @@
 'use strict'
 ;(function() {
   const mainWorldScript = function() {
-    let netflixPlayer
-
-    const customVideoSession = VideoSession => {
-      function CustomVideoSession() {
-        VideoSession.apply(this, arguments)
-        this._createPlayer = this.createPlayer
-        this.createPlayer = function() {
-          const player = this._createPlayer.apply(this, arguments)
-          netflixPlayer = player
-          return player
-        }
-      }
-      CustomVideoSession.prototype = VideoSession.prototype
-      return CustomVideoSession
+    const netflixPlayer = () => {
+      let player
+      try {
+        const { videoPlayer } = netflix.appContext.state.playerApp.getAPI()
+        const playerSessionId = videoPlayer.getAllPlayerSessionIds().find(Boolean)
+        player = videoPlayer.getVideoPlayerBySessionId(playerSessionId)
+      } catch (e) {}
+      return player || null
     }
-
-    var target = {}
-    var handler = {
-      set: function(target, propertyName, value, receiver) {
-        if (propertyName === 'player') {
-          value.VideoSession = customVideoSession(value.VideoSession)
-        }
-        target[propertyName] = value
-        return true
-      }
-    }
-
-    var p = new Proxy(target, handler)
-    window.netflix = p
 
     document.addEventListener('metastreamplay', e => {
       e.preventDefault()
-      if (netflixPlayer && netflixPlayer.getPaused()) {
-        netflixPlayer.play()
+      const player = netflixPlayer()
+      if (player && player.getPaused()) {
+        player.play()
       }
     })
 
     document.addEventListener('metastreampause', e => {
       e.preventDefault()
-      if (netflixPlayer && !netflixPlayer.getPaused()) {
-        netflixPlayer.pause()
+      const player = netflixPlayer()
+      if (player && !player.getPaused()) {
+        player.pause()
       }
     })
 
     document.addEventListener('metastreamseek', e => {
       e.preventDefault()
-      if (netflixPlayer) {
+      const player = netflixPlayer()
+      if (player) {
         const time = e.detail
-        netflixPlayer.seek(time)
+        player.seek(time)
       }
     })
   }
