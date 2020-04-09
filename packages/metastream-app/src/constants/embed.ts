b8989ef70e91cd3e8f8e9e223b7cd9c8fb8607e7
samuelmaddock <samuel.maddock@gmail.com>
2020-04-09 00:30:22 -0400

    fix: remove netflix from embed blocked list
    fixed by samesite changes in extension

--- packages/metastream-app/src/constants/embed.ts
+++ packages/metastream-app/src/constants/embed.ts
@@ -1,7 +1,7 @@
 /**
  * Set of domains which require loading in a top-level frame.
  */
-export const EMBED_BLOCKED_DOMAIN_LIST = new Set([
+export const EMBED_BLOCKED_DOMAIN_LIST = new Set<string>([
   // Blocked by SameSite cookie
-  'www.netflix.com'
+  // 'www.netflix.com'
 ])
