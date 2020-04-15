9f532260d0ea58f34387c597d84b6547d02deb66
samuelmaddock <samuel.maddock@gmail.com>
2020-04-14 21:43:41 -0400

    fix: disable translation of error stacktrace

--- packages/metastream-app/src/components/ErrorBoundary.tsx
+++ packages/metastream-app/src/components/ErrorBoundary.tsx
@@ -58,7 +58,7 @@ ${errorInfo ? errorInfo.componentStack : ''}
                 Discord community.
               </ExternalLink>
             </p>
-            <pre>{this.errorText}</pre>
+            <pre className="notranslate">{this.errorText}</pre>
             <p>
               <br />
               To continue, you can try reloading the application. If the problem persists, refer to
