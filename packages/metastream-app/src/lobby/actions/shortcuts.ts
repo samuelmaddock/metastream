fe7aac2215a3a2cd4b5aaaad6c8ecb070ff59cd0
samuelmaddock <samuel.maddock@gmail.com>
2020-05-08 01:04:30 -0400

    feat: add initial hotkey handlers
    play/pause, seek forward, seek backward

--- packages/metastream-app/src/lobby/actions/shortcuts.ts
+++ packages/metastream-app/src/lobby/actions/shortcuts.ts
@@ -1,26 +1,78 @@
 import { IAppState } from 'reducers'
 
-import { server_requestNextMedia, server_requestPlayPause } from 'lobby/actions/mediaPlayer'
+import { server_requestPlayPause, server_requestSeekRelative } from 'lobby/actions/mediaPlayer'
 import { AppThunkAction } from 'types/redux-thunk'
 import { ThunkDispatch } from 'redux-thunk'
 
-const dispatchCommand = (cmd: string, dispatch: ThunkDispatch<IAppState, any, any>) => {
-  switch (cmd) {
-    case 'media:next':
-      dispatch(server_requestNextMedia())
-      break
-    case 'media:playpause':
-      dispatch(server_requestPlayPause())
-      break
-  }
+const enum HotKeyAction {
+  PlayPause,
+  DecreaseVolume,
+  IncreaseVolume,
+  SeekBackward,
+  SeekForward
 }
 
+const ActionMap: { [key: string]: HotKeyAction } = {
+  Space: HotKeyAction.PlayPause,
+  ArrowDown: HotKeyAction.DecreaseVolume,
+  ArrowUp: HotKeyAction.IncreaseVolume,
+  ArrowLeft: HotKeyAction.SeekBackward,
+  ArrowRight: HotKeyAction.SeekForward
+}
+
+type ActionHandler = (dispatch: ThunkDispatch<IAppState, any, any>) => void
+
+// TODO: throttle handlers
+// TODO: clear throttle cooldowns on keyup
+const ActionHandlers: { [key in HotKeyAction]: ActionHandler } = {
+  [HotKeyAction.PlayPause]: dispatch => dispatch(server_requestPlayPause()),
+  [HotKeyAction.DecreaseVolume]: dispatch => {
+    /* TODO */
+  },
+  [HotKeyAction.IncreaseVolume]: dispatch => {
+    /* TODO */
+  },
+  // TODO: when held down for awhile, change relative seek to 1 minute, then 5 minutes
+  [HotKeyAction.SeekBackward]: dispatch => dispatch(server_requestSeekRelative(-5e3)),
+  [HotKeyAction.SeekForward]: dispatch => dispatch(server_requestSeekRelative(5e3))
+}
+
+function onDocumentKeyDown(
+  dispatch: ThunkDispatch<IAppState, any, any>,
+  getState: () => IAppState,
+  event: KeyboardEvent
+) {
+  // Ignore inputs entered while typing (ie. in the chat box)
+  const { activeElement } = document
+  if (activeElement instanceof HTMLInputElement || activeElement instanceof HTMLTextAreaElement) {
+    return
+  }
+
+  let hotkey = event.code
+  if (event.shiftKey) hotkey = `Shift+${hotkey}`
+
+  const action = ActionMap[hotkey]
+  if (typeof action === 'undefined') return
+
+  event.preventDefault()
+  event.stopImmediatePropagation()
+
+  const handler = ActionHandlers[action]
+  handler(dispatch)
+}
+
+let keyDownHandler: any
+
 export const registerMediaShortcuts = (): AppThunkAction => {
-  return () => {
-    // TODO: listen for media hotkeys
+  return (dispatch, getState) => {
+    keyDownHandler = onDocumentKeyDown.bind(null, dispatch, getState)
+    document.addEventListener('keydown', keyDownHandler, false)
   }
 }
 
 export const unregisterMediaShortcuts = (): AppThunkAction => {
-  return () => {}
+  return () => {
+    document.removeEventListener('keydown', keyDownHandler, false)
+    keyDownHandler = undefined
+  }
 }
