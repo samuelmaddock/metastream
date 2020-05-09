ad1015f73cbedeab41e26c433b08bd4ad857356a
samuelmaddock <samuel.maddock@gmail.com>
2020-05-09 01:47:03 -0400

    feat: increase scrub rate as key is held down

--- packages/metastream-app/src/lobby/actions/shortcuts.ts
+++ packages/metastream-app/src/lobby/actions/shortcuts.ts
@@ -4,6 +4,7 @@ import { server_requestPlayPause, server_requestSeekRelative } from 'lobby/actio
 import { AppThunkAction } from 'types/redux-thunk'
 import { ThunkDispatch } from 'redux-thunk'
 import { addVolume } from 'actions/settings'
+import { throttle } from 'lodash-es'
 
 const enum HotKeyAction {
   PlayPause,
@@ -21,24 +22,38 @@ const ActionMap: { [key: string]: HotKeyAction } = {
   ArrowRight: HotKeyAction.SeekForward
 }
 
-type ActionHandler = (dispatch: ThunkDispatch<IAppState, any, any>) => void
+type ActionHandler = (dispatch: ThunkDispatch<IAppState, any, any>, repeatDuration: number) => void
 
-// TODO: throttle handlers
-// TODO: clear throttle cooldowns on keyup
-const ActionHandlers: { [key in HotKeyAction]: ActionHandler } = {
-  [HotKeyAction.PlayPause]: dispatch => dispatch(server_requestPlayPause()),
-  [HotKeyAction.DecreaseVolume]: dispatch => dispatch(addVolume(-0.05)),
-  [HotKeyAction.IncreaseVolume]: dispatch => dispatch(addVolume(0.05)),
-  // TODO: when held down for awhile, change relative seek to 1 minute, then 5 minutes
-  [HotKeyAction.SeekBackward]: dispatch => dispatch(server_requestSeekRelative(-5e3)),
-  [HotKeyAction.SeekForward]: dispatch => dispatch(server_requestSeekRelative(5e3))
+// Increase rate as key is held down longer
+const getRateScalar = (durationMs: number) => {
+  if (durationMs > 5e3) return 5
+  else if (durationMs > 2e3) return 2
+  else return 1
 }
 
-function onDocumentKeyDown(
-  dispatch: ThunkDispatch<IAppState, any, any>,
-  getState: () => IAppState,
-  event: KeyboardEvent
-) {
+const ActionHandlers: { [key in HotKeyAction]: ActionHandler } = {
+  [HotKeyAction.PlayPause]: throttle(dispatch => dispatch(server_requestPlayPause()), 100, {
+    trailing: false
+  }),
+  [HotKeyAction.DecreaseVolume]: dispatch => dispatch(addVolume(-0.05)),
+  [HotKeyAction.IncreaseVolume]: dispatch => dispatch(addVolume(0.05)),
+  [HotKeyAction.SeekBackward]: throttle(
+    (dispatch, repeatDuration) =>
+      dispatch(server_requestSeekRelative(-5e3 * getRateScalar(repeatDuration))),
+    200,
+    { trailing: false }
+  ),
+  [HotKeyAction.SeekForward]: throttle(
+    (dispatch, repeatDuration) =>
+      dispatch(server_requestSeekRelative(5e3 * getRateScalar(repeatDuration))),
+    200,
+    { trailing: false }
+  )
+}
+
+const keyRepeatDuration: { [key: string]: number | undefined } = {}
+
+function onDocumentKeyDown(dispatch: ThunkDispatch<IAppState, any, any>, event: KeyboardEvent) {
   // Ignore inputs entered while typing (ie. in the chat box)
   const { activeElement } = document
   if (activeElement instanceof HTMLInputElement || activeElement instanceof HTMLTextAreaElement) {
@@ -48,6 +63,7 @@ function onDocumentKeyDown(
 
   let hotkey = event.code
   if (event.shiftKey) hotkey = `Shift+${hotkey}`
+  if (event.ctrlKey || event.metaKey) hotkey = `Ctrl+${hotkey}`
 
   const action = ActionMap[hotkey]
   if (typeof action === 'undefined') return
@@ -55,15 +71,21 @@ function onDocumentKeyDown(
   event.preventDefault()
   event.stopImmediatePropagation()
 
+  if (!event.repeat || !keyRepeatDuration[action]) {
+    keyRepeatDuration[action] = Date.now()
+  }
+
+  const repeatDuration = Date.now() - (keyRepeatDuration[action] || Date.now())
+
   const handler = ActionHandlers[action]
-  handler(dispatch)
+  handler(dispatch, repeatDuration)
 }
 
 let keyDownHandler: any
 
 export const registerMediaShortcuts = (): AppThunkAction => {
   return (dispatch, getState) => {
-    keyDownHandler = onDocumentKeyDown.bind(null, dispatch, getState)
+    keyDownHandler = onDocumentKeyDown.bind(null, dispatch)
     document.addEventListener('keydown', keyDownHandler, false)
   }
 }
