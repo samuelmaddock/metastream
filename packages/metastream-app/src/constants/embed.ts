08c9bd8235ef1e1066e174a5c4f32b5f99ce5774
samuelmaddock <samuel.maddock@gmail.com>
2020-05-16 19:58:01 -0400

    fix: only require external request tab in firefox

--- packages/metastream-app/src/constants/embed.ts
+++ packages/metastream-app/src/constants/embed.ts
@@ -1,7 +1,14 @@
+import { isFirefox } from 'utils/browser'
+
 /**
  * Set of domains which require loading in a top-level frame.
  */
-export const EMBED_BLOCKED_DOMAIN_LIST = new Set<string>([
-  // Blocked by SameSite cookie
-  // 'www.netflix.com'
-])
+export const EMBED_BLOCKED_DOMAIN_LIST = new Set<string>(
+  isFirefox()
+    ? [
+        // Blocked by SameSite cookie
+        // Waiting for FF addon to be approved for this fix
+        'www.netflix.com'
+      ]
+    : []
+)
