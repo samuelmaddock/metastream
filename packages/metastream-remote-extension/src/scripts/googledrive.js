b95e532a842237937d6ba20fd36d0e963b6ef08f
samuelmaddock <samuel.maddock@gmail.com>
2020-07-01 01:15:05 -0400

    fix: clear preview image on gdrive

--- packages/metastream-remote-extension/src/scripts/googledrive.js
+++ packages/metastream-remote-extension/src/scripts/googledrive.js
@@ -1,6 +1,16 @@
 window.addEventListener('load', () => {
-  const previewImg = document.querySelector('img:not([alt=""])')
-  if (previewImg) {
-    previewImg.click()
+  function clearPreview() {
+    const previewImg = document.querySelector('img:not([alt=""])')
+    if (previewImg) {
+      previewImg.click()
+      return true
+    }
   }
-})
+
+  const start = Date.now()
+  const intervalId = setInterval(() => {
+    if (clearPreview() || Date.now() - start > 10e3) {
+      clearInterval(intervalId)
+    }
+  }, 500)
+}, false)
