9f2354c69c6548d5581ec8def894dd21b2a2f391
samuelmaddock <samuel.maddock@gmail.com>
2020-07-21 21:44:24 -0400

    fix: username validation no longer removes numbers

--- packages/metastream-app/src/utils/string.ts
+++ packages/metastream-app/src/utils/string.ts
@@ -2,7 +2,11 @@ let EMOJI_REGEX: RegExp
 
 try {
   // Need to use constructor to avoid syntax parsing error in Firefox
-  EMOJI_REGEX = new RegExp('\\p{Emoji}', 'gu')
+  // https://mathiasbynens.be/notes/es-unicode-property-escapes
+  EMOJI_REGEX = new RegExp(
+    '\\p{Emoji_Modifier_Base}\\p{Emoji_Modifier}?|\\p{Emoji_Presentation}|\\p{Emoji}\\uFE0F',
+    'gu'
+  )
 } catch {
   // Fallback for browsers not supporting Unicode Property Escapes.
   EMOJI_REGEX = /([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2694-\u2697]|\uD83E[\uDD10-\uDD5D])/g
